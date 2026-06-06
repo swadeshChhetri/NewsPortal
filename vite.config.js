30cf084f0d2084a407491c3dc43a2f3c4cff1e98
Swadesh Chhetri <163563708+swadeshChhetri@users.noreply.github.com>
2026-06-06 23:27:23 +0530

    fix the frontend and backend issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const backendUrl = env.VITE_BASE_URL || 'https://int1b-newsportal.onrender.com';
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: backendUrl,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    },
+    preview: {
+      proxy: {
+        '/api': {
+          target: backendUrl,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    }
+  };
 })
+
